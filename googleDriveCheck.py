11bfd254c8d27371d1886f126f22e8f65a23c03d
Sean Huang <seanhuang2005@gmail.com>
2023-05-04 18:03:43 -0400

    final update

--- googleDriveCheck.py
+++ googleDriveCheck.py
@@ -7,8 +7,9 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-import trelloCreate
 from datetime import datetime
+import requests
+from creds import *
 
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 # the first array is the names of the people, the second array is the folder id for each person
@@ -16,6 +17,58 @@ folders = [
         ["Andrew","Bryson", "Daniel", "David", "Nelson", "Rachel", "Ronny", "Sean", "Shalott", "Tamir","Ted"],
         ["1zwyjtLaUkWRacN-gdxElDo05_51uFW39","1eFm2YnjMVWPYtUyEhbajBjC1-YloI2Vc","1EWYuzbLHvU8-wN3Nc7ncyvg9h1mjaaJC","1guIi7sZOaUMNRCvnB5bAmDqnxCQCPg7z","1PF4bmd1qW0pbKUIaAIXr58DvS27neLb7","1yf6IJK1YFVCN_BoW1ubK0BSdHfb_Vzxf","1C5XOYEKgUu0x_3CX5QpOMKIsQySUJuaI","1MEcU11EZs0YELzW2Kyd3bG30Lv63lv74","1ICoP3zL_mIkfmmr_2V0NoywsMgPOeItA","1nFMC-zVN58ljN546_i2x-OBxbHebDW7E","18-rwwoOtvAX4OYrwWR9zNrrIWmQnBCyf"]
         ]
+
+def createCard(link=None,name=None,fileName=None):
+    url = "https://api.trello.com/1/cards"
+    now = datetime.now()
+    date = now.strftime("%Y-%m-%d")
+    body = {
+        "key":KEY,
+        "token":TOKEN,
+        "idList":"64492238696b4da266e1c10e",
+        "name":"Reflection Video: "+("No file name" if not fileName else fileName),
+        "desc":f"""
+        
+    Link: {link}
+    Date Uploaded: {date}
+    Name: {name}""",
+    }
+
+    response = requests.request("POST",url,params=body)
+
+    data = response.json()
+    id = data['id']
+
+    ###### ADD CHECKLIST TO CARD ######
+    url = f"https://api.trello.com/1/cards/{id}/checklists"
+
+    body = {
+        "key": body["key"],
+        "token": body["token"],
+        "name": "Tasks",
+    }
+
+    response = requests.request("POST",url,params=body)
+
+
+    ##### Create checklist items #####
+    data = response.json()
+    id = data['id']
+
+    url = f"https://api.trello.com/1/checklists/{id}/checkItems"
+
+    tasks = ["Edit the video","Add Intro","Add Outro","Add Special Effects","Upload to Social Media"]
+    for i in range(len(tasks)):
+        body = {
+            "key": body["key"],
+            "token": body["token"],
+            "name": tasks[i],
+            "pos": "bottom"
+        }
+
+        response = requests.request("POST",url,params=body)
+
+        print(response.status_code)
 
 
 def main():
@@ -55,7 +108,7 @@ def main():
 
                 #if the file was created today, print it
                 if datetime.strptime(created_time, '%Y-%m-%dT%H:%M:%S.%fZ').date() == datetime.today().date():
-                    trelloCreate.createCard("https://drive.google.com/file/d/"+file_id, folders[0][i], file_name)
+                    createCard("https://drive.google.com/file/d/"+file_id, folders[0][i], file_name)
                     print(f"New file found: {file_name} (ID: {file_id}, created at {created_time})")
                 else:
                     print(f"File found: {file_name} (ID: {file_id}, created at {created_time})")
@@ -71,15 +124,3 @@ def main():
     return "OK"
 
 if __name__ == "__main__": main()
-
-
-
-#TODO:
-# *DONE*  ----- remove the while loop and create two arrays (the arrays will contain name and folder id for each person), loops through it
-# *DONE*  ----- time check to see if it was uploaded today
-# *DONE*  ----- get folder id from everyone (get folder link from everyone)
-# *DONE*  ----- associate folder id with name
-
-#NOTE:
-# authorization needs to be refreshed often
-# this program should be ran at night because it checks if the upload date is the current daynvm
